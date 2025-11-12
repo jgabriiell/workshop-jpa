3e13fe52b750b97dc11e87cd9970e67d14b416dc
João Gabriel <jgabriiell@gmail.com>
2025-11-12 09:59:59 -0300

    Order, instant, ISO 8601 pt. 2

--- src/main/java/com/personal/course/entities/User.java
+++ src/main/java/com/personal/course/entities/User.java
@@ -1,5 +1,6 @@
 package com.personal.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +30,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
 
---
+++ src/main/java/com/personal/course/services/OrderService.java
@@ -0,0 +1,23 @@
+package com.personal.course.services;
+
+import com.personal.course.entities.Order;
+import com.personal.course.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Order findById(Long id) {
+        return repository.findById(id).get();
+    }
+}
