aec0dc9e45d5e7bada7958b60bb3fa68f73a5d0e
João Gabriel <jgabriiell@gmail.com>
2025-11-27 09:59:08 -0300

    Product

---
+++ src/main/java/com/personal/course/services/ProductService.java
@@ -0,0 +1,23 @@
+package com.personal.course.services;
+
+import com.personal.course.entities.Product;
+import com.personal.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        return repository.findById(id).get();
+    }
+}
