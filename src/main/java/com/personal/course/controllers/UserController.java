1c20d09f78fb51f99bc8ec26c2d1bef2882046d3
João Gabriel <jgabriiell@gmail.com>
2025-12-02 21:22:30 -0300

    User delete

--- src/main/java/com/personal/course/controllers/UserController.java
+++ src/main/java/com/personal/course/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.personal.course.entities.User;
 import com.personal.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,5 +40,11 @@ public class UserController {
          user = service.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
          return  ResponseEntity.created(uri).body(user);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
